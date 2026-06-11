47c2094fb0498060de80844ded90d2c8f9f1f2c5
Ane-18 <aneabadro@gmail.com>
2026-06-11 14:11:29 +0200

    sw: network-first strategy, always serve fresh content

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE = 'fp-dual-v3';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+const CACHE = 'fp-dual-v4';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => clients.claim())
+));
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    const clone = res.clone();
-    caches.open(CACHE).then(c => c.put(e.request, clone));
-    return res;
-  })));
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
 });
